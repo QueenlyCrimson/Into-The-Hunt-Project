493e8b31f533ff78a5ebfe8e32826e6d46d8d28d
QueenlyCrimson <ccinemainn@gmail.com>
2023-07-17 13:10:17 -0500

    made components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <header>
+        <NavBar />
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+
+  return(
+  
+    <header className="nav">
+      <nav></nav>
+    </header>
+  
+  )
+}
+
+export default NavBar
