858ca39def761176c86d0b0aec5a55818d327280
QueenlyCrimson <ccinemainn@gmail.com>
2023-07-27 20:35:34 -0500

    Chapter.jsx created

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -3,7 +3,9 @@ const Book = () => {
   return(
     
       <div className="book">
-        This is a book.
+        <p>Into The Dark</p>
+        <img src="https://www.fanfiction.net/image/6787522/75/" alt="into the dark"></img>
+        <span></span>
       </div>
     
   )
