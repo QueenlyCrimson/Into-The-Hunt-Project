88e5fce811eb6f475379e943600351565c511363
QueenlyCrimson <ccinemainn@gmail.com>
2023-07-28 14:26:42 -0500

    Bookpage page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import BookPage from "./pages/BookPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/book" element={<BookPage />} />
         </Routes>
       </main>
     </div>
