67445bfbfbe1e8fd1def770bd5ee464a335fefdd
QueenlyCrimson <ccinemainn@gmail.com>
2023-08-04 22:17:18 -0500

    mvp complete

---
+++ client/src/components/PrevPage.jsx
@@ -0,0 +1,13 @@
+
+
+const PrevPage = ({ chapterNumber }) => {
+  if (chapterNumber >= 2) {
+    console.log(
+    "balls"
+    )
+  }
+
+  return(<div></div>)
+}
+
+export default PrevPage
--- client/src/pages/BookPage.js
+++ client/src/pages/BookPage.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import PrevPage from "../components/PrevPage";
 var parse = require("html-react-parser");
 
 const BookPage = () => {
   let { id } = useParams();
 
-  let chapterNumber = 1;
-
   const [chapter, setChapter] = useState([]);
+  let [chapterNumber, setChapterNumber] = useState([]);
   const [chapterContent, setChapterContent] = useState([]);
   const [book, setBook] = useState([]);
+
+  const startFunction = () => {
+    setChapterNumber(1);
+    handleBook();
+  };
 
   const handleBook = async () => {
     const bookName = await axios.get(`http://localhost:3001/api/books/${id}`);
@@ -22,22 +27,42 @@ const BookPage = () => {
     const chapter = await axios.get(
       `http://localhost:3001/api/chapter/${title}/${chapterNumber}`
     );
-    console.log(chapter.data.chapter[0].book_name);
-    setChapter(chapter.data);
+
+    // setChapter(chapter.data);
     const preprocessedText = chapter.data.chapter[0].text;
     let processedText = preprocessedText.replaceAll("\\", "");
     setChapterContent(processedText);
   };
 
-  useEffect(() => {
+  const nextChapter = () => {
+    setChapterNumber(chapterNumber++);
+    console.log(chapterNumber);
     handleBook();
+  };
+
+  const previousChapter = () => {
+    setChapterNumber(chapterNumber - 1);
+    console.log(chapterNumber);
+    handleBook();
+  };
+
+  useEffect(() => {
+    startFunction();
   }, []);
+
   return (
     <div>
       <div
         className="bookContent"
         dangerouslySetInnerHTML={{ __html: chapterContent }}
       ></div>
+
+      <div className="whitetext" onClick={nextChapter}>
+        Next Chapter
+      </div>
+      <div className="whitetext" onClick={previousChapter}>
+        Previous Chapter
+      </div>
       <div>
         <Link to={`/addchapter/${id}`}>Add Chapter</Link>
       </div>
