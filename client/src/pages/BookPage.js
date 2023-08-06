399fb8b079cd7067f368f1081a4a74a2125afe13
QueenlyCrimson <ccinemainn@gmail.com>
2023-08-06 15:03:36 -0500

    changed something cus i wanna cry

--- client/src/pages/BookPage.js
+++ client/src/pages/BookPage.js
@@ -13,7 +13,7 @@ const BookPage = () => {
   const [book, setBook] = useState([]);
 
   const startFunction = () => {
-    setChapterNumber(1);
+    setChapterNumber("1");
     handleBook();
   };
 
